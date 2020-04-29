ebc5105875a019a59328c1d0455fe0f2edb1d06d
Paul Sicking <sickingp@gmail.com>
2020-04-29 08:17:09 -0700

    Added mpld3 plot

--- app/views.py
+++ app/views.py
@@ -1,8 +1,28 @@
  # app/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.staticfiles import finders
+import matplotlib.pyplot as plt
+import mpld3
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+
+        '''
+        fig, ax = plt.subplots()
+        plt.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
+        fig_html = mpld3.fig_to_html(fig)
+        '''
+
+        html_file_path = finders.find("app/img/Carolyn3.html")
+        searched_locations = finders.searched_locations
+        f = open(html_file_path, "r")
+        fig_html = f.read()
+        print(fig_html)
+        return render(request, 
+                      'index.html',
+                        {
+                            'title' : "Family Viewer About",
+                            'content' : fig_html
+                        })
